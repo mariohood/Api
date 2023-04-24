d21c893a9f5f5399a980d51a20a5035883fb482d
mariohood <manhattan199@hotmail.com>
2023-04-23 22:54:14 -0300

    criado User

---
+++ src/main/java/br/com/mariohood/api/domain/User.java
@@ -0,0 +1,22 @@
+package br.com.mariohood.api.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+    
+    
+}
