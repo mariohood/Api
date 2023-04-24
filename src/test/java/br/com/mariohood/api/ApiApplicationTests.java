67a2ebe99e8163f85be9482f5c0d609b658f5f69
mariohood <manhattan199@hotmail.com>
2023-04-23 22:13:21 -0300

    Projeto Teste Api iniciado

---
+++ src/test/java/br/com/mariohood/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mariohood.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
