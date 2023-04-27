bab38b03518da41794fa46be8d1e4966727dc80e
mariohood <manhattan199@hotmail.com>
2023-04-27 00:39:13 -0300

    Revert "criado User"
    This reverts commit d21c893a9f5f5399a980d51a20a5035883fb482d.

--- src/main/java/br/com/mariohood/api/ApiApplication.java
+++ src/main/java/br/com/mariohood/api/ApiApplication.java
@@ -3,15 +3,11 @@ package br.com.mariohood.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.mariohood.api.domain.User;
-
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
-
-		User user = new User(1, "Mario", "mario@email.com", "1234"); 
 	}
 
 }
